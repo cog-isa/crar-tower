19fc4112c0b3fd79c5990d95d3481ee43111fa24
agorodetskiy <gorodetskiyandrew@gmail.com>
2020-07-07 23:00:20 +0300

    relax tf gpu memory requirement

--- examples/test_CRAR/run_catcher.py
+++ examples/test_CRAR/run_catcher.py
@@ -8,6 +8,8 @@ import numpy as np
 from joblib import hash, dump
 import os
 
+import tensorflow as tf
+
 from deer.default_parser import process_args
 from deer.agent import NeuralAgent
 from deer.learning_algos.CRAR_keras import CRAR
@@ -15,6 +17,18 @@ from catcher_env import MyEnv as catcher_env
 import deer.experiment.base_controllers as bc
 
 from deer.policies import EpsilonGreedyPolicy
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 
 
 class Defaults:
